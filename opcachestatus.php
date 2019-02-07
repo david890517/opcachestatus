dad8c6c33191c8c50768c4972d1322ed2e081e52
Darko <dev-101@users.noreply.github.com>
2019-02-07 10:58:45 +0100

    HTML + CSS improvements

--- opcachestatus.php
+++ opcachestatus.php
@@ -16,19 +16,101 @@ if (isset($_POST['opcache_reset'])) {
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP OpCache Statistics</title>
-		<style>
-			*{
-				text-align:left;
-			}
-			table,th,td{
-				padding:3px;
-				background-color:#F5F5F5;
-			}
-			table td{
-				border:1px solid #DCDCDC;
-			}
-		</style>
+<title>PHP OpCache Statistics</title>
+<style>
+*{
+text-align:left;
+margin:0;
+padding:0;
+}
+table{
+width:100%;
+}
+table,th,td{
+padding:3px;
+background-color:#F5F5F5;
+}
+table td{
+border:1px solid #DCDCDC;
+}
+
+table.scripts{
+white-space:nowrap;
+}
+table.scripts tr{
+}
+table.scripts td{
+}
+
+table.scripts th.numb{
+white-space:nowrap;
+}
+table.scripts th.path{
+max-width:625px;
+overflow:auto;
+}
+table.scripts th.hits{
+text-align:right;
+white-space:nowrap;
+}
+table.scripts th.memo{
+text-align:right;
+white-space:nowrap;
+}
+table.scripts th.date{
+text-align:right;
+white-space:nowrap;
+}
+
+table.scripts td.numb{
+white-space:nowrap;
+}
+table.scripts td.path{
+max-width:625px;
+overflow:auto;
+}
+table.scripts td.hits{
+text-align:right;
+white-space:nowrap;
+}
+table.scripts td.memo{
+text-align:right;
+white-space:nowrap;
+}
+table.scripts td.date{
+text-align:right;
+white-space:nowrap;
+}
+
+div#fixed-window-wrapper{
+position:fixed;
+margin:0;
+padding:5px;
+bottom:5px;
+right:5px;
+z-index:2;
+opacity:0.5;
+box-sizing:border-box;
+border:1px solid #CCC;
+background-color:#EEE;
+}
+div#fixed-window-wrapper:hover{
+opacity:1.0;
+}
+div#fixed-window-wrapper a{
+display:block;
+margin:2px 0;
+padding:2px;
+background-color:#DDD;
+text-decoration:none;
+}
+div#fixed-window-wrapper div#quick-links{
+font-size:14px;
+margin-bottom:5px;
+}
+div#fixed-window-wrapper div#reset-cache{
+}
+</style>
 	</head>
 	<body>
 
@@ -49,9 +131,7 @@ $config = opcache_get_configuration();
 #######################
 
 if(!$status) {
-
 	echo 'OPCode Caching is not enabled on this system.';
-
 } else {
 
 	// array debug
@@ -104,11 +184,11 @@ if(!$status) {
 # DATA DISPLAY #
 ################
 
-// Wrapper
+// Open Wrapper
 echo '<div style="width:100%; max-width:1000px; margin:0 auto;">';
 
 // Main Title
-echo '<table width="100%">';
+echo '<table id="main">';
 	echo '<thead>';
 		echo '<tr>';
 			echo '<th colspan="3"><span style="color:#F40;">' . $opcacheStatus . '</span></th>';
@@ -129,7 +209,7 @@ if($status) {
 	echo '<br/><br/>';
 
 	// Memory
-	echo '<table width="100%">';
+	echo '<table id="memory">';
 		echo '<thead>';
 			echo '<tr>';
 				echo '<th colspan="3"><span style="color:#F40;">' . 'MEMORY USAGE' . '</span></th>';
@@ -162,7 +242,7 @@ if($status) {
 	echo '<br/><br/>';
 
 	// OpCache Hit/Miss Ratio
-	echo '<table width="100%">';
+	echo '<table id="hit-miss">';
 		echo '<thead>';
 			echo '<tr>';
 				echo '<th colspan="3"><span style="color:#F40;">' . 'OPCACHE HIT/MISS RATIO' . '</span></th>';
@@ -183,10 +263,10 @@ if($status) {
 	echo '<br/><br/>';
 
 	// OpCache Stats
-	echo '<table width="100%">';
+	echo '<table id="statistics">';
 		echo '<thead>';
 			echo '<tr>';
-				echo '<th colspan="3"><span style="color:#F40;">' . 'OPCACHE STATS' . '</span></th>';
+				echo '<th colspan="3"><span style="color:#F40;">' . 'OPCACHE STATISTICS' . '</span></th>';
 			echo '</tr>';
 			echo '<tr>';
 				echo '<th>Parameter</th>';
@@ -206,7 +286,7 @@ if($status) {
 	echo '<br/><br/>';
 
 	// OpCache Directives
-	echo '<table width="100%">';
+	echo '<table id="directives">';
 		echo '<thead>';
 			echo '<tr>';
 				echo '<th colspan="3"><span style="color:#F40;">' . 'OPCACHE DIRECTIVES' . '</span></th>';
@@ -229,28 +309,28 @@ if($status) {
 	echo '<br/><br/>';
 
 	// OpCache Scripts
-	echo '<table width="100%">';
+	echo '<table id="scripts" class="scripts">';
 		echo '<thead>';
 			echo '<tr>';
 				echo '<th colspan="6"><span style="color:#F40;">' . 'OPCACHE SCRIPTS' . '</span></th>';
 			echo '</tr>';
 			echo '<tr>';
-				echo '<th>N</th>';
-				echo '<th>Full Path</th>';
-				echo '<th>Hits</th>';
-				echo '<th>Memory [KB]</th>';
-				echo '<th>Last Used</th>';
+				echo '<th class="numb">N</th>';
+				echo '<th class="path">Full Path</th>';
+				echo '<th class="hits">Hits</th>';
+				echo '<th class="memo">Memory [KB]</th>';
+				echo '<th class="date">Last Used</th>';
 			echo '</tr>';
 		echo '</thead>';
 		echo '<tbody>';
 			$i = 1;
 			foreach ($opcacheScripts as $key) {
 				echo '<tr>';
-					echo '<td>' . $i . '</td>';
-					echo '<td>' . $key['full_path'] . '</td>';
-					echo '<td>' . $key['hits'] . '</td>';
-					echo '<td>' . round(($key['memory_consumption'] / 1024), 2) . '</td>';
-					echo '<td>' . $key['last_used'] . '</td>';
+					echo '<td class="numb">' . $i . '</td>';
+					echo '<td class="path">' . $key['full_path'] . '</td>';
+					echo '<td class="hits">' . $key['hits'] . '</td>';
+					echo '<td class="memo">' . round(($key['memory_consumption'] / 1024), 2) . '</td>';
+					echo '<td class="date">' . $key['last_used'] . '</td>';
 				echo '</tr>';
 				$i++;
 			}
@@ -261,7 +341,7 @@ if($status) {
 		echo '<br/><br/>';
 
 		// OpCache Blacklist
-		echo '<table width="100%">';
+		echo '<table id="blacklist">';
 			echo '<thead>';
 				echo '<tr>';
 					echo '<th colspan="3"><span style="color:#F40;">' . 'OPCACHE BLACKLIST' . '</span></th>';
@@ -280,22 +360,32 @@ if($status) {
 }
 
 echo '<br/>';
-
 ?>
-
-<form name="opcache_reset" id="opcache_reset" action="opcachestatus.php" method="post">
-	<input type="hidden" name="opcache_reset" value="opcache_reset"/>
-	<button type="submit" style="padding:8px 10px;">RESET CACHE</button>
-</form>
-
+<div id="fixed-window-wrapper">
+	<!-- Quick Jump Links -->
+	<div id="quick-links">
+	<p><a href="#main">OpCache Main</a></p>
+	<p><a href="#memory">OpCache Memory</a></p>
+	<p><a href="#hit-miss">OpCache Hit&#47;Miss</a></p>
+	<p><a href="#statistics">OpCache Statistics</a></p>
+	<p><a href="#directives">OpCache Directives</a></p>
+	<p><a href="#scripts">OpCache Scripts</a></p>
+	<?php if ($opcacheBlacklist) { ?>
+	<p><a href="#blacklist">OpCache Blacklist</a></p>
+	<?php } ?>
+	</div>
+	<!-- Reset Cache Button -->
+	<div id="reset-cache">
+	<form name="opcache_reset" id="opcache_reset" action="opcachestatus.php" method="post">
+		<input type="hidden" name="opcache_reset" value="opcache_reset"/>
+		<button type="submit" style="padding:8px 10px;">RESET CACHE</button>
+	</form>
+	</div>
+</div>
+<?php
+	// Close Wrapper
+	echo '</div>';
+}
+?>
 	</body>
 </html>
-
-<?php
-
-// Close Wrapper
-echo '</div>';
-
-}
-
-?>
